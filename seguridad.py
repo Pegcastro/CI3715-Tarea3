9078ae35eee8ede4c9d561b05e8a04fded35ddc0
Pablo Gonzalez <pagoncastro@gmail.com>
2020-02-02 14:59:21 -0400

    Construcción de la función de verificar email

--- seguridad.py
+++ seguridad.py
@@ -1,15 +1,29 @@
-
+import re
 class Seguridad:
 
     def __init__(self):
         self.dict = {}
-    
-    def registrarUsuario(self, email, password, password2):
-        pass
+        self.regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        
+    def _verEmail(self, email):
+        match = re.search(self.regex, email)
+
+        if(match):
+            return True
+        else:
+            return False
+
+    def registrarUsuario(self, email, password, password2):        
+        return self._verEmail(email)
 
     def ingresarUsuario(self, email, password):
         pass
 
+
+
+
+    def _verPass(self):
+        pass
 
 if __name__ == '__main__':
     pass
