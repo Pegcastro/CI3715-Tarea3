d7ac3e288882795359ea1e0a6dab96c376cafeb9
Pablo Gonzalez <pagoncastro@gmail.com>
2020-02-08 21:36:44 -0400

    Final pruebas tests.py

--- tarea3/login/tests.py
+++ tarea3/login/tests.py
@@ -7,5 +7,22 @@ class SeguridadModelTests(TestCase):
     def test_caso_frontera_email_registro_1(self):
         self.assertIs(registrarUsuario('a', 'aAa12345', 'aAa12345')[0], False)
 
+    def test_caso_frontera_email_registro_2(self):
+        self.assertIs(registrarUsuario('pagoncastro@gmail.com', 'Pablito1', 'Pablito1')[0], True)
+
+    def test_caso_frontera_pass_registro_2(self):
+        self.assertIs(registrarUsuario('pagoncastro@gmail.com', 'a', 'a')[0], True)
+
+    def test_caso_frontera_pass_registro_2(self):
+        self.assertIs(registrarUsuario('pagoncastro@gmail.com', 'Pablito1', 'Pablito2')[0], False)
+
     def test_interno_ingreso_1(self):
-        self.assertIs(ingresarUsuario('almiavicas@gmail.com', 'Cont1234')[0], True)
+        self.assertIs(ingresarUsuario('pagoncastro@gmail.com', 'Pablito1')[0], True)
+    
+    def test_interno_ingreso_2(self):
+        self.assertIs(ingresarUsuario('pagoncastro@gmail.com', 'Pablito2')[0], False)
+
+    def test_interno_ingreso_3(self):
+        self.assertIs(ingresarUsuario('almiavicas@gmail.com', 'Pablito1')[0], False)
+
+    
